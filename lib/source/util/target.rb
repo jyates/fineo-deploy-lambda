d264592936272086c910ee3f00d160204e3694cd
Jesse Yates <jesse.k.yates@gmail.com>
2016-08-17 11:31:03 -0700

    target.opts -> args

--- lib/source/util/target.rb
+++ lib/source/util/target.rb
@@ -3,20 +3,20 @@ require 'build/arg_opt'
 
 class Target
 
-  attr_reader :name, :jar
+  attr_reader :name, :jar, :args
 
   def initialize(name, jar)
     @name = name
     @jar = jar
-    @opts = []
+    @args = []
   end
 
   def option(property_name, default_value, description)
-     opts << ArgOpts.simple(property_name, default_value, description)
+     args << ArgOpts.simple(property_name, default_value, description)
   end
 
   def test_prefixable_option(property_name, default_name, description)
-    opts << ArgOpts.prefix(property_name, default_value, description)
+    args << ArgOpts.prefix(property_name, default_value, description)
   end
 
   def properties?(prefix)
