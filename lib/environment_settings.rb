c8b13f75f3428cac30a650ff55a0fe7113c4ca6c
Jesse Yates <jesse@fineo.io>
2016-09-30 11:55:56 -0700

    Fixing reading environment to fit jenkins variables

--- lib/environment_settings.rb
+++ lib/environment_settings.rb
@@ -1,25 +1,23 @@
 
 module EnvironmentSettings
-  
 
-def simplify(hash)
-  hash.select{|k,v|
-    !v.nil?
-  }
-end
+  def simplify(hash)
+    hash.select{|k,v|
+      !v.nil?
+    }
+  end
 
-def append(arr, env_name, name)
-  value = ENV[env_name]
-  skip = value.nil? || value == false
-  inc = !skip || @options.all
-  arr << name if inc
-end
+  def append(arr, env_name, name)
+    value = ENV[env_name]
+    inc = @options.all || value == "true"
+    arr << name if inc
+  end
 
-def as_input(input, name, dir_key)
-  return nil if input.empty?
-  dir = ENV[dir_key]
-  raise "No source directory specified for #{name} => ENV[#{dir_key}]!" if dir.nil?
-  info = {"dir" => dir, "targets" => input}
-  {name => { "info" => info}}
-end
+  def as_input(input, name, dir_key)
+    return nil if input.empty?
+    dir = ENV[dir_key]
+    raise "No source directory specified for #{name} => ENV[#{dir_key}]!" if dir.nil?
+    info = {"dir" => dir, "targets" => input}
+    {name => { "info" => info}}
+  end
 end
